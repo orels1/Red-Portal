7c9567d42a0026613819b1fadf7e07073b23d3c0
orels1 <orel-s1@yandex.ru>
2017-01-17 05:05:55 +0300

    updated apidocs dev server

--- docs.js
+++ docs.js
@@ -7,7 +7,6 @@ var app = express();
 
 // App Middleware
 app.set('port', process.env.PORT || 3500);
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 'extended': false }));
 app.use(express.static(path.join(__dirname, 'docs')));
