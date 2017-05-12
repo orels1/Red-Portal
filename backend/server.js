9f0c49a2cdfc955344d30ab7eca6596551a927b9
orels1 <orel-s1@yandex.ru>
2017-05-12 20:26:33 +0300

    basic setup

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send({
-        'status': 'OK',
-        'results': 'api is alive',
-    });
+  res.status(200).send({
+    status: 'OK',
+    results: 'api is alive',
+  });
+
 });
 
 app.listen(3000, () => {
-    console.info('Running on port 3000');
+  console.info('Running on port 3000');
+});
+
+app.use(function onError(err, req, res, next) {
+  res.status(500).send({
+    status: 'ERROR',
+    error: err.message,
+    error_code: err.code || null,
+  });
 });
