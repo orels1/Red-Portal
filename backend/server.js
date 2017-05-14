a3057519a3b33d9918c431779200156ed9811c38
orels1 <orel-s1@yandex.ru>
2017-05-14 20:46:44 +0300

    async errors catching and mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to a db
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.mongoURL || 'localhost/redportal');
+
+// Routes
+app.use('/repos', require('./repos'));
 
 app.get('/', (req, res) => {
   res.status(200).send({
@@ -14,10 +22,17 @@ app.listen(3000, () => {
   console.info('Running on port 3000');
 });
 
-app.use(function onError(err, req, res, next) {
-  res.status(500).send({
-    status: 'ERROR',
-    error: err.message,
-    error_code: err.code || null,
-  });
+app.use((err, req, res, next) => {
+  switch (err.message) {
+    case 'NotFound':
+      return res.status(404).send({
+        status: 'ERROR',
+        error: err.message,
+      });
+    default:
+      return res.status(500).send({
+        status: 'ERROR',
+        error: err.message,
+      });
+  }
 });
---
+++ backend/utils.js
@@ -0,0 +1,15 @@
+/**
+ * Created by orel- on 14/May/17.
+ */
+
+// async/await error catcher
+const catchAsyncErrors = fn => (
+  (req, res, next) => {
+    const routePromise = fn(req, res, next);
+    if (routePromise.catch) {
+      routePromise.catch(err => next(err));
+    }
+  }
+);
+
+exports.catchAsync = catchAsyncErrors;
