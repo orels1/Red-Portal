3f426175ec6fe5f4508e8de7e34256848d846022
orels1 <orel-s1@yandex.ru>
2017-05-14 03:00:09 +0300

    created general pathing file

---
+++ backend/paths.js
@@ -0,0 +1,15 @@
+/**
+ * Created by orel- on 14/May/17.
+ * Stores all the api paths for use inside modules and schemas
+ */
+const API_PATH = '/api/v2/';
+
+exports.API_PATH = API_PATH;
+
+exports.REPOS_PATH = `${API_PATH}repos/`;
+
+exports.COGS_PATH = `${API_PATH}cogs/`;
+
+exports.USERS_PATH = `${API_PATH}users/`;
+
+exports.WEBHOOKS_PATH = `${API_PATH}wh/`;
