195647f18ee40d6b5fcf200fc88fe2bea4c9cde8
orels1 <orel-s1@yandex.ru>
2017-09-04 04:02:37 +0300

    basic parser #44

---
+++ backend/parser/index.js
@@ -0,0 +1,59 @@
+const Promise = require('bluebird');
+const _ = require('lodash');
+const { listCogs, repoInfo, cogInfo, repoReadme, cogReadme } = require('../github');
+
+const TOKEN = process.env.GITHUB_TOKEN;
+
+const parseRepo = async (username, repo, token = TOKEN, branch = 'master') => {
+    const repoJson = await repoInfo(username, repo, branch, TOKEN);
+
+    const readme = await repoReadme(username, repo, branch, TOKEN);
+
+    let { cogs, broken } = await parseCogs(username, repo);
+
+    const cogsReadme = await Promise.all(
+        cogs.map(
+            c => cogReadme(username, repo, c.name, branch, TOKEN)
+        )
+    );
+
+    cogs = cogs.map((c, index) => Object.assign(c, {
+        readme: cogsReadme[index]
+    }));
+
+    return {
+        repo: repoJson,
+        readme: readme,
+        cogs,
+        broken
+    };
+};
+
+exports.parseRepo = parseRepo;
+
+const parseCogs = async (username, repo, token = TOKEN, branch = 'master') => {
+
+    let cogs = await listCogs(username, repo, token);
+    
+    const cogsInfo = await Promise.all(
+        cogs.map(
+            c => cogInfo(username, repo, c, branch, token)
+        )
+    );
+
+    cogs = cogs.map((c, index) => ({
+        name: c,
+        info: cogsInfo[index],
+    }));
+
+    let broken = [];
+
+    cogs = _.compact(_.map(cogs, c => {
+        if (c.info !== null) return c;
+        broken.push(c.name);
+    }));
+
+    return { cogs, broken }
+}
+
+exports.parseCogs = parseCogs;
