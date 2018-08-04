908b2f72e10f375b206e1945c0eba3b6104fd84b
orels1 <orel-s1@yandex.ru>
2018-08-05 01:20:39 +0300

    updated docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,15 +1,1 @@
-define({
-  "title": "Red-Portal API",
-  "url": "https://cogs.red/api/v1",
-  "sampleUrl": "http://localhost:3000/api/v1",
-  "name": "red-portal",
-  "version": "1.0.0",
-  "description": "A cog-listing project for Red-DiscordBot",
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2017-04-08T23:19:05.649Z",
-    "url": "http://apidocjs.com",
-    "version": "0.16.1"
-  }
-});
+define({  "title": "Red-Portal API",  "url": "https://cogs.red/api/v1",  "sampleUrl": "http://localhost:3000/api/v1",  "name": "red-portal",  "version": "1.0.0",  "description": "A cog-listing project for Red-DiscordBot",  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2018-08-04T22:20:28.967Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
